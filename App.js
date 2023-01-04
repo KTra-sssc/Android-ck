59b5d813b9328fc1f08de5b515ff59fe9d641b62
KTra <83985873+KTra-sssc@users.noreply.github.com>
2023-01-05 05:59:20 +0700

    bottom-tab-bar

---
+++ App.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import FontAwesome from 'react-native-vector-icons/FontAwesome'
+
+import Home from './screens/Home';
+import Search from './screens/Search';
+import Favorite from './screens/Favorite';
+import Profile from './screens/Profile';
+
+
+const Tab = createBottomTabNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+        tabBarOptions={{
+          activeTintColor: 'black',
+          inactiveTintColor: '#575757',
+        }}
+        screenOptions={{
+            title: '',
+            headerShown: false
+        }}>
+        <Tab.Screen
+          name="Home"
+          component={Home}
+          options={{
+            tabBarLabel: 'Trang chủ',
+            tabBarIcon: ({color}) => (
+              <FontAwesome name="home" size={30} color={color} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Search"
+          component={Search}
+          options={{
+            tabBarLabel: 'Tìm kiếm',
+            tabBarIcon: ({color}) => (
+              <FontAwesome name="search"size={30} color={color} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Favorite"
+          component={Favorite}
+          options={{
+            tabBarLabel: 'Yêu thích',
+            tabBarIcon: ({color}) => (
+              <FontAwesome name="heart" size={30} color={color} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Profile"
+          component={Profile}
+          options={{
+            tabBarLabel: 'Cá nhân',
+            tabBarIcon: ({color}) => (
+              <FontAwesome name="user" size={30} color={color} />
+            ),
+          }}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
--- index.js
+++ index.js
@@ -5,5 +5,5 @@
 import {AppRegistry} from 'react-native';
 import {name as appName} from './app.json';
 import React from 'react';
-import Home from './screens/Home'
-AppRegistry.registerComponent(appName, () => () => <Home/>);
+import App from './App';
+AppRegistry.registerComponent(appName, () => () => <App/>);
